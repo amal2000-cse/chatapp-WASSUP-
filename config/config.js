bb7ed7403be4d475066176197d11c3421b85fe50
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2023-12-20 11:54:59 +0530

    mongodb link updated

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connect=async()=>{
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/chatAppNew",{
+    await mongoose.connect("mongodb+srv://amal:suvarnam123@cluster0.he09jpu.mongodb.net/?retryWrites=true&w=majority",{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
